9834302b130182c2acd3b193bedaac207581c591
misalolupraveenkumar-dev <misalolupraveenkumar@outlook.com>
2024-04-30 22:52:44 +0530

    Pounds To Kilogram Converter code added

---
+++ src/main/java/info/com/general/PoundsToKilogramConverter.java
@@ -0,0 +1,11 @@
+package info.com.general;
+
+public class PoundsToKilogramConverter {
+    public static void main(String[] args) {
+
+        double pounds = 100;
+        double kilograms = pounds * 0.45359237;
+
+        System.out.println("Pounds:  "+ pounds + "   kilograms:  "+kilograms );
+    }
+}
